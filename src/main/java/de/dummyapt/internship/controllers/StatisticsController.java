30ee7c41196653b6c761137b672c1db86a53bc32
Dummyapt <duha.cinar@gmail.com>
2021-06-21 13:06:46 +0200

    Updates and bug fixes.

--- src/main/java/de/dummyapt/internship/controllers/StatisticsController.java
+++ src/main/java/de/dummyapt/internship/controllers/StatisticsController.java
@@ -1,12 +1,24 @@
 package de.dummyapt.internship.controllers;
 
+import de.dummyapt.internship.services.api.StatisticsServiceAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StatisticsController {
+    @Autowired
+    private StatisticsServiceAPI statisticsService;
+
     @GetMapping("/stats")
-    public String showStats() {
+    public String showStats(Model model) {
+        model.addAttribute("SHP", statisticsService.getStatisticsCountBy(1));
+        model.addAttribute("Spot", statisticsService.getStatisticsCountBy(2));
+        model.addAttribute("Flex", statisticsService.getStatisticsCountBy(3));
+        model.addAttribute("Base", statisticsService.getStatisticsCountBy(4));
+        model.addAttribute("Flex2", statisticsService.getStatisticsCountBy(5));
+        model.addAttribute("Peak", statisticsService.getStatisticsCountBy(6));
         return "stats";
     }
 }
