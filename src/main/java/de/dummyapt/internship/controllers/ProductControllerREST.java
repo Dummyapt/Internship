26ca709073c9a168b96bcf1dbebe0995edd75a89
Dummyapt <duha.cinar@gmail.com>
2021-06-22 04:50:46 +0200

    Updates and bug fixes.

--- src/main/java/de/dummyapt/internship/controllers/ProductControllerREST.java
+++ src/main/java/de/dummyapt/internship/controllers/ProductControllerREST.java
@@ -4,6 +4,7 @@ import de.dummyapt.internship.models.Product;
 import de.dummyapt.internship.services.api.ProductServiceAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -63,4 +64,7 @@ public class ProductControllerREST {
                 </table>""");
         return stringBuilder.toString();
     }
+
+    @PostMapping("/confirmOrder")
+    public void confirmOrder() {}
 }
