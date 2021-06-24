4fcf98f8d8487f9861be86502e8e034632a21b0e
Dummyapt <duha.cinar@gmail.com>
2021-06-24 15:28:44 +0200

    Updates and bug fixes.

--- src/main/java/de/dummyapt/internship/models/Commodity.java
+++ src/main/java/de/dummyapt/internship/models/Commodity.java
@@ -1,6 +1,5 @@
 package de.dummyapt.internship.models;
 
-import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
@@ -11,14 +10,8 @@ import javax.persistence.*;
 public class Commodity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull
     @Column(name = "id")
     private Integer id;
-    @NotNull
     @Column(name = "name")
     private String name;
-
-    public Commodity(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/de/dummyapt/internship/models/Product.java
+++ src/main/java/de/dummyapt/internship/models/Product.java
@@ -1,6 +1,5 @@
 package de.dummyapt.internship.models;
 
-import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
@@ -11,23 +10,13 @@ import javax.persistence.*;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull
     @Column(name = "id")
     private Integer id;
-    @NotNull
     @Column(name = "name")
     private String name;
-    @NotNull
     @Column(name = "current_price")
     private Double currentPrice;
-    @NotNull
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "commodity_id")
     private Commodity commodity;
-
-    public Product(String name, Double currentPrice, Commodity commodity) {
-        this.name = name;
-        this.currentPrice = currentPrice;
-        this.commodity = commodity;
-    }
 }
